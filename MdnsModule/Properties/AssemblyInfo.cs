46f0eefc65b1270f15147b8b183659fb80834914
johnathandavis <johnathan.davis@outlook.com>
2015-03-22 15:00:01 -0500

    Added PS Module

--- MdnsModule/Properties/AssemblyInfo.cs
+++ MdnsModule/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ba7e6fda-d6d1-4855-8063-c2112aef69b0")]
+[assembly: Guid("ab8905b6-e532-4400-b011-b50ac14d6d4f")]
 
 // Version information for an assembly consists of the following four values:
 //
